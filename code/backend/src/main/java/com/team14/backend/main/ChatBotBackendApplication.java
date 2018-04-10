b9747c638fa27bbfc65e69467a4b0b1e7d7cf511
Florian Widder <florian.widder@live.de>
2018-04-10 15:48:11 +1200

    Added Backend Skeleton

---
+++ code/backend/src/main/java/com/team14/backend/main/ChatBotBackendApplication.java
@@ -0,0 +1,12 @@
+package com.team14.backend.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatBotBackendApplication.class, args);
+	}
+}
---
+++ code/backend/src/test/java/com/team14/backend/main/ChatBotBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.team14.backend.main;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChatBotBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

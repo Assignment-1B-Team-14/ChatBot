1df9ea89ead924129513ed55865c9bb0bd67f6ec
Florian Widder <florian.widder@live.de>
2018-04-21 23:26:09 +1200

    DEBUG

--- travis/upload.sh
+++ travis/upload.sh
@@ -4,7 +4,6 @@ sudo chmod 777 -R $HOME
 
 ##### Create Output Folders #####
 cd $HOME
-mkdir builds
 mkdir builds/Web-Page
 mkdir builds/Android-App
 mkdir builds/iOS-App
@@ -31,27 +30,15 @@ git config --global user.email "florian.widder@live.de"
 git config --global user.name "Florian Widder"
 
 ##### Clone Master Branch ######
-cd $HOME/builds
-echo 0
-dir
 git clone --quiet --branch master https://fwidder:$OAUTH_GITHUB@github.com/Assignment-1B-Team-14/ChatBot $HOME/master
-echo 1
-dir
 
 ##### Copy Files #####
-cd $HOME/builds
-echo 2
-dir
 mv -f $HOME/builds $HOME/master/build
-echo 3
-dir
 
 ##### Upload to Git #####
 cd $HOME/master/
-echo 4
-dir
 git add -f ./build/*
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed. [skip ci]"
-git push origin builds -fq
+git push origin master:builds -fq
 
 echo "Done!"

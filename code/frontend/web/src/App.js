8e7e63ed8e366cd5653e4eb2f9e57a1ed280c0f6
pacMan <schoolbag007@gmail.com>
2018-04-23 16:42:57 +1200

    First Steps with React.js

--- code/frontend/web/src/App.js
+++ code/frontend/web/src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'reactstrap';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Container>
+      <Col  xs="10" />
+          <Col>
+            Text
+          </Col>
+        <Col  />
+      </Container>
       </div>
     );
   }
